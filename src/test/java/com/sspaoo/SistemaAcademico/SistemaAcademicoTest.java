063f2234e06003d8d5c81fce9f5e55fd4111b528
Alusc <lukasknoppamaral@gmail.com>
2025-07-13 18:56:50 -0300

    esqueleto de teste

---
+++ src/test/java/com/sspaoo/SistemaAcademico/SistemaAcademicoTest.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.sspaoo.SistemaAcademico;
+
+iimport com.sspaoo.Aluno.Aluno;
+import com.sspaoo.Turma.Turma;
+mport org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author lukas
+ */
+public class SistemaAcademicoTest {
+    
+    public SistemaAcademicoTest() {
+    }
+    @org.junit.jupiter.api.BeforeAll
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    public void tearDown() throws Exception {
+    }
+
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public v    }
+
+    /**
+     * Test of main method, of class SistemaAcademico.
+     */
+    @org.junit.jupiter.api.Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        SistemaAcademico.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of matricularAluno method, of class SistemaAcademico.
+     */
+    @org.junit.jupiter.api.Test
+    public void testMatricularAluno() {
+        System.out.println("matricularAluno");
+        Aluno aluno = null;
+        SistemaAcademico.matricularAluno(aluno);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of realizarMatricula method, of class SistemaAcademico.
+     */
+    @org.junit.jupiter.api.Test
+    public void testRealizarMatricula() throws Exception {
+        System.out.println("realizarMatricula");
+        Aluno aluno = null;
+        Turma turma = null;
+        SistemaAcademico.realizarMatricula(aluno, turma);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of validarMatricula method, of class SistemaAcademico.
+     */
+    @org.junit.jupiter.api.Test
+    public void testValidarMatricula() {
+        System.out.println("validarMatricula");
+        Aluno aluno = null;
+        Turma turma = null;
+        SistemaAcademico.validarMatricula(aluno, turma);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+oid tearDown() {
+    }
+    
+}
